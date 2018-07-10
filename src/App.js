bbcfbe88793fab9ed0d540e0eec437a7838c9479
Molamin <molamin73@gmail.com>
2018-07-10 14:55:55 +0100

    added few packages and header template

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import React, { Component } from 'react';
 import './core/app.js';
-
-import logo from './logo.svg';
+import NavBar from './components/NavBar';
+ 
 import './App.css';
-import { Provider } from "react-redux";
+// eslint-disable-next-line
 import { store } from "./store.js";
-
+//t
 class VGApp extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React hooked with redux</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <NavBar />
       </div>
     );
   }
 
-}
+  _stateChanged(props){
 
-const mapStateToProps = state => {
-  return {
-    todos: state.app.name
   }
+
 }
 
 export default VGApp;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,5 @@ import './index.css';
 import VGApp from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { Provider } from "react-redux";
-import { store } from "./store.js";
-
-ReactDOM.render(
-    <Provider store="{store}">
-        <VGApp />
-    </Provider>, 
-    document.getElementById('root')
-);
+ReactDOM.render(<VGApp />, document.getElementById('root'));
 registerServiceWorker();
